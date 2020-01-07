5e37d529ece7ea0b31de497bf676c3288dd7350d
xieqiupeng <xieqiupeng@gmail.com>
2020-01-08 01:04:51 +0800

    udpate

--- Enum-LuoZhenYu/dependency.py
+++ Enum-LuoZhenYu/dependency.py
@@ -7,7 +7,7 @@ ln -sf /Volumes/Project/Enum/Enum-Subject/罗辑思维  $Path/罗辑思维
 ln -sf /Volumes/Project/Enum/Enum-Subject/时间的朋友  $Path/时间的朋友
 
 # Root
-Path=/Volumes/Project/Rep/LuoZhenYu
+Path=/Volumes/Project/Rep/LuoZhenYu/Enum-LuoZhenYu
 ln -sf /Volumes/Project/Enum/Enum-Subject/罗辑思维  $Path/罗辑思维
 ln -sf /Volumes/Project/Enum/Enum-Subject/时间的朋友  $Path/时间的朋友
 
